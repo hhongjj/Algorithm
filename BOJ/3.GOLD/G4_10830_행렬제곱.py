598edbfe546fa29b3df66852604f63989d952945
hhongjj <hhongjj3@gmail.com>
2021-09-21 23:37:08 +0900

    0921 sol

---
+++ BOJ/3.GOLD/G4_10830_행렬제곱.py
@@ -0,0 +1,7 @@
+# G5 10830 행렬 제곱
+# https://www.acmicpc.net/problem/10830
+# 수학, 분할 정복, 분할 정복을 이용한 거듭제곱, 선형대수학
+
+N, B = map(int, input().split())
+A = list(list(map(int, input().split())) for _ in range(N))
+print(A)
