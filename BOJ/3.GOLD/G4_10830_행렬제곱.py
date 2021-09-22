8633b9c658c8646f0105318806927dc64d3c35f5
hhongjj <hhongjj3@gmail.com>
2021-09-22 21:23:14 +0900

    0922 sol

--- BOJ/3.GOLD/G4_10830_행렬제곱.py
+++ BOJ/3.GOLD/G4_10830_행렬제곱.py
@@ -1,7 +1,37 @@
 # G5 10830 행렬 제곱
 # https://www.acmicpc.net/problem/10830
 # 수학, 분할 정복, 분할 정복을 이용한 거듭제곱, 선형대수학
+#
+
+def multi(c, d):
+    ans = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                ans[i][j] += c[i][k] * d[k][j]
+            ans[i][j] %= 1000
+    return ans
+
+
+def matrix(b, mat):
+    if b == 1:
+        return mat
+    elif b == 2:
+        return multi(mat, mat)
+    else:
+        temp = matrix(b//2, mat)
+        if b % 2 == 0:
+            return multi(temp, temp)
+        else:
+            return multi(multi(temp, temp), mat)
+0
 
 N, B = map(int, input().split())
 A = list(list(map(int, input().split())) for _ in range(N))
-print(A)
+
+res = matrix(B, A)
+
+for i in range(N):
+    for j in range(N):
+        print(res[i][j], end=' ')
+    print('')
---
+++ BOJ/4.SILVER/S2_11047_동전0.py
@@ -0,0 +1,6 @@
+# S2 11047 동전 0
+# https://www.acmicpc.net/problem/11047
+# 그리디 알고리즘
+
+N, K = map(int, input().split())
+money = 
