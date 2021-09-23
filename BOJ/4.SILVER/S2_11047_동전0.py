93f9b2202e933c71ef10abaaa013823827c213ea
hhongjj <hhongjj3@gmail.com>
2021-09-23 10:43:56 +0900

    0922 sol

--- BOJ/4.SILVER/S2_11047_동전0.py
+++ BOJ/4.SILVER/S2_11047_동전0.py
@@ -2,5 +2,22 @@
 # https://www.acmicpc.net/problem/11047
 # 그리디 알고리즘
 
+from collections import deque
+
 N, K = map(int, input().split())
-money = 
+money = deque()
+for _ in range(N):
+    money.append(int(input().strip()))
+
+cnt = 0
+while K:
+    temp = money.pop()
+    if K // temp:
+        cnt += K // temp
+        K -= temp * (K // temp)
+
+print(cnt)
+
+
+# 큰 동전 부터 K을 나눌 수 있다면 몫 만큼 횟수를 더하고,
+# K에서 뺄 수 있는 만큼 빼는 것을 반복한다.
---
+++ BOJ/4.SILVER/S2_1931_회의실배정.py
@@ -0,0 +1,4 @@
+# S2 1931 회의실 배정
+# https://www.acmicpc.net/problem/1931
+# 그리디 알고리즘, 정렬
+
