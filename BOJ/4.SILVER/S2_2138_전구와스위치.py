146940b2c308b85820dd6e2a2acb23042dd0418e
hhongjj <hhongjj3@gmail.com>
2021-09-29 01:15:52 +0900

    0928 sol

--- BOJ/4.SILVER/S2_2138_전구와스위치.py
+++ BOJ/4.SILVER/S2_2138_전구와스위치.py
@@ -1,8 +1,47 @@
 # S2 2138 전구와 스위치
 # https://www.acmicpc.net/problem/2138
 # 그리디 알고리즘
+import sys
+input = sys.stdin.readline
 
 N = int(input())
+A = list(map(int, input().rstrip()))
+B = list(map(int, input().rstrip()))
+
+
+def change(k, n):
+    for j in range(k, k+n):
+        A[j] = 1 - A[j]
+
+
+cnt = 0
+i = 0
+while i < N:
+    if i < N-2 and A[i] == B[i] and A[i+1] == B[i+1] and A[i+2] == B[i+2]:
+        i += 3
+    if i == 0:
+        if A[i] == B[i] and A[i+1] == B[i+1]:
+            i += 2
+        elif A[i] == B[i] and A[i+1] != B[i+1]:
+            change(i, 2)
+            i += 1
+            cnt += 1
+    if A[i-1] != B[i-1]:
+        change(i-1, 3)
+        i += 1
+        cnt += 1
+    if i == N-1:
+        if A[i-1] != B[i-1] and A[i-1] != B[i-1]:
+            change(i-1, 2)
+            cnt += 1
+            break
+        else:
+            break
+
+if A == B:
+    print(cnt)
+else:
+    print(-1)
 
 
 
---
+++ BOJ/4.SILVER/S4_1783_병든나이트.py
@@ -0,0 +1,77 @@
+# S4 1783 병든 나이트
+# https://www.acmicpc.net/problem/1783
+# 구현, 그리디 알고리즘, 많은 조건 분기
+# 틀림.
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def check_small():
+    stack = deque()
+    stack.append((0, 0))
+    knight[0][0] = 1
+    num = 1
+    while stack:
+        r, c = stack.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if nr < 0 or nr >= N or nc < 0 or nc >= M:
+                continue
+            if knight[nr][nc] < knight[r][c] + 1:
+                stack.append((nr, nc))
+                knight[nr][nc] = knight[r][c] + 1
+            if num < knight[nr][nc]:
+                num = knight[nr][nc]
+    return num
+
+
+def check_big():
+    stack = deque()
+    stack.append((0, 0))
+    knight[0][0] = 1
+    num = 1
+    for i in range(4):
+        r, c = stack.popleft()
+        nr = r + dr[i]
+        nc = c + dc[i]
+        if nr < 0 or nr >= N or nc < 0 or nc >= M:
+            continue
+        if knight[nr][nc] < knight[r][c] + 1:
+            stack.append((nr, nc))
+            knight[nr][nc] = knight[r][c] + 1
+        if num < knight[nr][nc]:
+            num = knight[nr][nc]
+    if not len(stack):
+        stack.append((r, c))
+    while stack:
+        r, c = stack.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if nr < 0 or nr >= N or nc < 0 or nc >= M:
+                continue
+            if knight[nr][nc] < knight[r][c] + 1:
+                stack.append((nr, nc))
+                knight[nr][nc] = knight[r][c] + 1
+            if num < knight[nr][nc]:
+                num = knight[nr][nc]
+    return num
+
+
+N, M = map(int, input().split())
+if N >= M * 2:
+    N = M * 2
+knight = [[0]*M for _ in range(N)]
+dc = [1, 1, 2, 2]
+dr = [2, -2, 1, -1]
+
+if M <= 4:
+    res = check_small()
+    print(res)
+else:
+    res = check_big()
+    print(res)
+
