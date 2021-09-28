2461b056d2498066103b088d0f7cd29357cf11e9
hhongjj <hhongjj3@gmail.com>
2021-09-29 02:14:48 +0900

    sol S2 1541

--- BOJ/4.SILVER/S2_1541_잃어버린괄호.py
+++ BOJ/4.SILVER/S2_1541_잃어버린괄호.py
@@ -14,23 +14,23 @@ for i in lst:
             stack.append(i)
     else:
         stack.append(i)
+# 연산자를 기준으로 연산자와 함께 stack에 넣는다.  deque(['55', '-50', '+40'])
+
 result = int(stack.popleft())
 minus = 0
 while stack:
     num = stack.popleft()
-    if num[0] == '-':
+    if num[0] == '-':                  # 만약 -라면 그다음 -가 나올때 까지 숫자를 다 minus에 더한다.
         minus += int(num[1:])
         while stack:
             num = stack.popleft()
             if num[0] == '+':
                 minus += int(num[1:])
-            else:
+            else:                     # - 가 나오면 다시 stack에 넣어 준다.
                 stack.appendleft(num)
                 break
-    else:
+    else:                             # -가 바로 앞에 없다면 그냥 result에 더해준다.
         result += int(num[1:])
 
+# result값과 minus 값을 빼준다.
 print(result-minus)
-
-
-
