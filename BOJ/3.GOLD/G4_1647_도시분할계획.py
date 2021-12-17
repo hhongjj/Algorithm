2044ce92392c3ce4c8735b1c04ad1b303d3d98e6
hhongjj <hhongjj3@gmail.com>
2021-12-17 23:32:58 +0900

    1647

--- BOJ/3.GOLD/G4_1647_도시분할계획.py
+++ BOJ/3.GOLD/G4_1647_도시분할계획.py
@@ -1,4 +1,9 @@
 # G4 1647 도시 분할 계획
 # https://www.acmicpc.net/problem/1647
 # 그래프 이론, 최소 스패닝 트리
-
+import operator
+N, M = map(int, input().split())
+edge = list(list(map(int, input().split())) for _ in range(M))
+edge.sort(key=lambda x: x[2])
+# edge.sort(key=operator.itemgetter(2))
+print(edge)
