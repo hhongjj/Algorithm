90cd253de6146184a5b273859a0d92e79d9d844d
hhongjj <hhongjj3@gmail.com>
2022-04-10 15:45:08 +0900

    ing 18119

--- BOJ/3.GOLD/G4_18119_단어암기.py
+++ BOJ/3.GOLD/G4_18119_단어암기.py
@@ -5,26 +5,71 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-words = []
-check = list([0]*(26) for _ in range(N))
+alpha = [set() for _ in range(26)]
 for i in range(N):
-    word = input().rstrip()
-    for j in range(97, 123):
-        if chr(j) in word:
-            check[i][j-97] = 1
-    
-    words.append((word, sum(check[i])))
-    
+    word = set(input().rstrip())
+    for w in word:
+        alpha[ord(w)-97] |= set([i])
 
+one = []
+result = set(list(i for i  in range(N)))
 for _ in range(M):
     x, y = input().split()
-    cnt = 0 
-    for i in range(N):
-        if y in words[i][0]:
-            check[i][ord(y)-97] = ord(x) - 49
-        if words[i][1] == sum(check[i]):
-            cnt += 1
+    if x == '1':
+        result -= alpha[ord(y)-97]
+        one.append(y)
+    else:
+        one.remove(y)
+        result = set(list(i for i  in range(N)))
+        for o in one:
+            result -= alpha[ord(o)-97]
+    print(len(result))
 
-    print(cnt)
+
+# 두번째 방법
+# N, M = map(int, input().split())
+# words = list(input().rstrip() for _ in range(N))
+# alpha = []
+# for i in range(97,123):
+#     tmp = []
+#     for idx, j in enumerate(words):
+#         if chr(i) in j:
+#             tmp.append(idx)
+#     alpha.append(set(tmp))
+
+# one = []
+# result = set(list(i for i  in range(N)))
+# for _ in range(M):
+#     x, y = input().split()
+#     if x == '1':
+#         result -= alpha[ord(y)-97]
+#         one.append(y)
+#     else:
+#         one.remove(y)
+#         result = set(list(i for i  in range(N)))
+#         for o in one:
+#             result -= alpha[ord(o)-97]
+#     print(len(result))
 
 
+# 첫번째 방법
+# N, M = map(int, input().split())
+# words = []
+# check = list([0]*(26) for _ in range(N))
+# for i in range(N):
+#     word = input().rstrip()
+#     for j in range(97, 123):
+#         if chr(j) in word:
+#             check[i][j-97] = 1
+#     words.append((word, sum(check[i])))
+
+# for _ in range(M):
+#     x, y = input().split()
+#     cnt = 0 
+#     for i in range(N):
+#         if y in words[i][0]:
+#             check[i][ord(y)-97] = ord(x) - 49
+
+#         if words[i][1] == sum(check[i]):
+#             cnt += 1
+#     print(cnt)
